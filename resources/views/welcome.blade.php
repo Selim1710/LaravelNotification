23b77a68e1785dd5654cbc90b3341a37cfc12051
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-04-02 15:07:26 +0600

    Uses of Queue for improving mail sending speed(php artisan queue:table , php artisan migrate, go to app>>notification>>EmailNotification then implement queue, php artisan queue:work)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -439,7 +439,7 @@
                             </svg>
                             <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Message</a></div>
                         </div>
-
+                        <!-- message -->
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                 @if( session()->has('message'))
